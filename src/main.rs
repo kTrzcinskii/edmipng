1d7138117d0a6c9c0a4f1cb58e457bc099620fa7
kTrzcinskii <kt.trzcinski@gmail.com>
2024-09-05 17:28:52 +0200

    Add cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
+use args::Args;
+use clap::Parser;
 
+mod args;
 mod chunk;
 mod chunk_type;
 mod png;
 
 fn main() -> Result<()> {
-    todo!()
+    let args = Args::parse();
+    Ok(())
 }
